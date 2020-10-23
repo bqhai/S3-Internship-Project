05d4bf321c48c2280a46a0d059e095f02c4563ce
Bui Nguyen Quang Hai <65085715+bqhai@users.noreply.github.com>
2020-10-23 17:13:44 +0700

    Update DbCellPhoneStore.sql

--- ScriptDatabase/DbCellPhoneStore.sql
+++ ScriptDatabase/DbCellPhoneStore.sql
@@ -3,81 +3,11 @@ GO
 
 USE DbCellPhoneStore
 GO
-
-CREATE TABLE Brand
-(
-	BrandID NVARCHAR(50) PRIMARY KEY,
-	BrandName NVARCHAR(100),
-	Country NVARCHAR(100),
-)
-GO
-
-CREATE TABLE Smartphone
-(
-	SmartphoneID NVARCHAR(50) PRIMARY KEY,
-	BrandID NVARCHAR(50),
-	Origin NVARCHAR(100), 
-	Accessories NVARCHAR(500),
-	ScreenType NVARCHAR(500),
-	ScreenSize NVARCHAR(100),
-	ScreenResolution NVARCHAR(100),
-	FrontCamera NVARCHAR(100),
-	BackCamera NVARCHAR(100),
-	OperatingSystem NVARCHAR(100),
-	Chipset NVARCHAR(100),
-	CPU NVARCHAR(100),
-	GPU NVARCHAR(100),
-	RAM NVARCHAR(100),
-	ROM NVARCHAR(100),
-	SDCard NVARCHAR(100) DEFAULT N'Không hỗ trợ',
-	Network NVARCHAR(200),
-	WLAN NVARCHAR(100),
-	Bluetooth NVARCHAR(100),
-	GPS NVARCHAR(100),
-	NFC NVARCHAR(100) DEFAULT N'Không hỗ trợ',
-	USB NVARCHAR(100),
-	Sensor NVARCHAR(500),
-	Battery NVARCHAR(100),
-	Status NVARCHAR(100),
-	Image NVARCHAR(500),
-	
-	FOREIGN KEY(BrandID) REFERENCES Brand(BrandID)
-)
-GO
-
-CREATE TABLE SmartphoneVersion
-(
-	SmartphoneVersionID NVARCHAR(50) PRIMARY KEY,
-	SmartphoneID NVARCHAR(50),
-	RAM NVARCHAR(100),
-	ROM NVARCHAR(100),
-	Color NVARCHAR(100),
-	Price INT,
-	QuantityInStock INT,
-
-	FOREIGN KEY(SmartphoneID) REFERENCES Smartphone(SmartphoneID)
-)
-GO
-
-CREATE TABLE Orders
-(
-	OrderID NVARCHAR(50) PRIMARY KEY,
-	Payments NVARCHAR(100),
-	Delivery NVARCHAR(100),
-	Notes NVARCHAR(500),
-	OrderDate DATE,
-	TotalPrice INT,
-	CustomerID NVARCHAR(50),
-	Status BIT, --0: 
-
-
-)
-GO
-
+-------------CREATE TABLE----------------
 CREATE TABLE Role
 (
 	RoleID NVARCHAR(50) PRIMARY KEY,
-	RoleName NVARCHAR(100),
+	RoleName NVARCHAR(100) NOT NULL,
 )
 GO
 
@@ -122,3 +52,153 @@ CREATE TABLE Employee
 	FOREIGN KEY(Username) REFERENCES Account(Username)
 )
 GO
+CREATE TABLE Brand
+(
+	BrandID NVARCHAR(50) PRIMARY KEY,
+	BrandName NVARCHAR(100),
+	Country NVARCHAR(100),
+)
+GO
+
+CREATE TABLE Smartphone
+(
+	SmartphoneID NVARCHAR(50) PRIMARY KEY,
+	Name NVARCHAR(100),
+	BrandID NVARCHAR(50),
+	Size NVARCHAR(100),
+	Weight NVARCHAR(100),
+	ScreenType NVARCHAR(500),
+	ScreenSize NVARCHAR(100),
+	ScreenResolution NVARCHAR(100),
+	FrontCamera NVARCHAR(100),
+	BackCamera NVARCHAR(100),
+	OperatingSystem NVARCHAR(100),
+	Chipset NVARCHAR(100),
+	CPU NVARCHAR(100),
+	GPU NVARCHAR(100),
+	SDCard NVARCHAR(100) DEFAULT N'Không hỗ trợ',
+	Network NVARCHAR(200),
+	WLAN NVARCHAR(100),
+	Bluetooth NVARCHAR(100),
+	GPS NVARCHAR(100),
+	NFC NVARCHAR(100) DEFAULT N'Không hỗ trợ',
+	USB NVARCHAR(100),
+	SIM NVARCHAR(100),
+	Sensor NVARCHAR(500),
+	Battery NVARCHAR(100),
+	Image NVARCHAR(500),
+	
+	FOREIGN KEY(BrandID) REFERENCES Brand(BrandID)
+)
+GO
+
+CREATE TABLE SmartphoneVersion
+(
+	SmartphoneVersionID NVARCHAR(50) PRIMARY KEY,
+	SmartphoneID NVARCHAR(50),
+	RAM NVARCHAR(100),
+	ROM NVARCHAR(100),
+	Color NVARCHAR(100),
+	Price INT,
+	QuantityInStock INT,
+	Status NVARCHAR(100),
+	
+	FOREIGN KEY(SmartphoneID) REFERENCES Smartphone(SmartphoneID)
+)
+GO
+
+CREATE TABLE Laptop
+(
+	LaptopID NVARCHAR(50) PRIMARY KEY,
+	Name NVARCHAR(100),
+	BrandID NVARCHAR(50),
+	Size NVARCHAR(100),
+	Weight NVARCHAR(100),
+	ScreenType NVARCHAR(500),
+	ScreenSize NVARCHAR(100),
+	ScreenResolution NVARCHAR(100),
+	OperatingSystem NVARCHAR(100),
+	OSVersion NVARCHAR(100),	
+	WLAN NVARCHAR(100),
+	Bluetooth NVARCHAR(100),
+	Port NVARCHAR(100),
+	Battery NVARCHAR(100),
+	Image NVARCHAR(500),
+
+	FOREIGN KEY(BrandID) REFERENCES Brand(BrandID)
+)
+GO
+
+CREATE TABLE LaptopVersion
+(
+	LaptopVersionID NVARCHAR(50) PRIMARY KEY,
+	LaptopID NVARCHAR(50),
+	CPU NVARCHAR(100),
+	VGA NVARCHAR(100),
+	RAM NVARCHAR(100),
+	HardDrive NVARCHAR(100),
+	Price INT,
+	QuantityInStock INT,
+	Status NVARCHAR(100),
+
+	FOREIGN KEY(LaptopID) REFERENCES Laptop(LaptopID)
+)
+GO
+
+CREATE TABLE Orders
+(
+	OrderID NVARCHAR(50) PRIMARY KEY,
+	Payments NVARCHAR(100),
+	Delivery NVARCHAR(100),
+	Notes NVARCHAR(500),
+	OrderDate DATE,
+	TotalPrice INT,
+	CustomerID NVARCHAR(50),
+	Status BIT, --0: Unpaid --1: Paid
+	
+	FOREIGN KEY(CustomerID) REFERENCES Customer(CustomerID)
+)
+GO
+
+CREATE TABLE OrderDetail
+(
+	OrderID NVARCHAR(50),
+	ProductVersionID NVARCHAR(50),
+	Amount INT,
+	Price INT,
+
+	PRIMARY KEY(OrderID, ProductVersionID),
+	FOREIGN KEY(OrderID) REFERENCES Orders(OrderID),
+	FOREIGN KEY(ProductVersionID) REFERENCES SmartphoneVersion(SmartphoneVersionID),
+	FOREIGN KEY(ProductVersionID) REFERENCES LaptopVersion(LaptopVersionID)
+
+)
+GO
+
+CREATE TABLE ProductIntroduce
+(
+	ProductID NVARCHAR(50) PRIMARY KEY,
+	Header1 NVARCHAR(MAX),
+	Text1 NVARCHAR(MAX),
+	Image1 NVARCHAR(100),
+	Header2 NVARCHAR(MAX),
+	Text2 NVARCHAR(MAX),
+	Image2 NVARCHAR(100),
+	Header3 NVARCHAR(MAX),
+	Text3 NVARCHAR(MAX),
+	Image3 NVARCHAR(100),
+	Header4 NVARCHAR(MAX),
+	Text4 NVARCHAR(MAX),
+	Image4 NVARCHAR(100),
+	Header5 NVARCHAR(MAX),
+	Text5 NVARCHAR(MAX),
+	Image5 NVARCHAR(100),
+	Text6 NVARCHAR(MAX),
+	Image7 NVARCHAR(100),
+
+	FOREIGN KEY(ProductID) REFERENCES Smartphone(SmartphoneID),
+	FOREIGN KEY(ProductID) REFERENCES Laptop(LaptopID)
+)
+GO
+
+-------------INSERT DATA----------------
